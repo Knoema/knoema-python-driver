a4815a06da04608ada6398aef535c0c204c2cb2e
chirkova <chirkova@knoema.com>
2018-01-29 14:23:03 +0500

    KN-37425 version up

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 setup(
   name = 'knoema',
   packages = ['knoema'],
-  version = '1.0.12b1',
+  version = '1.0.13b1',
   description = "Official Python package for Knoema's API",
   long_description=readme(),
   author = 'Knoema',

d24b76a0babf8998595a1222f44c078080afbef0
Aleksey Filippov <afilippov@knoema.com>
2020-09-04 15:28:00 +0500

    KN-79857 data API change

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 setup(
   name = 'knoema',
   packages = ['knoema'],
-  version = '1.2.21',
+  version = '1.2.25b1',
   description = "Official Python package for Knoema's API",
   long_description=readme(),
   author = 'Knoema',

63ac7f9f39af65f9befb0e814571d786ab9795da
Vyacheslav Lopaev <vlopaev@knoema.com>
2021-10-20 13:27:02 +0500

    ET-151 Python search function returns error

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 setup(
   name = 'knoema',
   packages = ['knoema'],
-  version = '1.2.40',
+  version = '1.2.41',
   description = "Official Python package for Knoema's API",
   long_description=readme(),
   author = 'Knoema',

81d78db04b5ef2df186a85bc766b150f077d95e5
Vitaliy Popov <popov@knoema.com>
2021-12-30 11:29:29 +0500

    ET-129  name parameter in python.upload does not work for files

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 setup(
   name = 'knoema',
   packages = ['knoema'],
-  version = '1.2.42',
+  version = '1.2.43',
   description = "Official Python package for Knoema's API",
   long_description=readme(),
   author = 'Knoema',
